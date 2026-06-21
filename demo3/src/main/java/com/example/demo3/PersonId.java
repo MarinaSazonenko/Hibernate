1836d42cc7b552cfce131f8db6b0cbdaeec6fbba
Marina <marina.torlen@gmail.com>
2026-06-21 19:06:43 +0500

    Update PersonId.java

--- demo3/src/main/java/com/example/demo3/PersonId.java
+++ demo3/src/main/java/com/example/demo3/PersonId.java
@@ -1,9 +1,13 @@
 package com.example.demo3;
 
 import java.io.Serializable;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.EqualsAndHashCode;
 
-@Data
+@Getter
+@Setter
+@EqualsAndHashCode // Безопасно для класса-ключа, так как здесь нет связей и ленивой загрузки
 public class PersonId implements Serializable {
     private String name;
     private String surname;
